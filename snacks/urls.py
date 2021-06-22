c38217d6a610deaa1f6967806c512d11b854985b
Zach Hornung <Zachhornung@comcast.net>
2021-06-22 16:07:50 -0700

    basic django is wired up

---
+++ snacks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import HomeView, AboutView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path("home/", HomeView.as_view(), name="home"),
+    path("about/", AboutView.as_view(), name="about"),
+    # path("", ThingListView.as_view(), name="thing_list"),
+    # path("<int:pk>/", ThingDetailView.as_view(), name="thing_detail"),
+]
