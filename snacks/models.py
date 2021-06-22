b11dfffb4b5e5fd32af9c4830986074bc91be5be
Zach Hornung <Zachhornung@comcast.net>
2021-06-22 16:55:51 -0700

    added some snacks and a nice picture of coffee

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.contrib.auth import get_user_model
 # Create your models here.
+class Snack(models.Model):
+  name = models.CharField(max_length=64)
+  purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  description = models.TextField()
+  
+  def __str__(self):
+    return self.name
